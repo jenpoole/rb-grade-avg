8c33ca21633fe1cc4c3a6d900bff529926b8a9b9
Jen <contact.vandepool@gmail.com>
2016-04-12 20:03:08 +0000

    get user input

---
+++ user_input.rb
@@ -0,0 +1,33 @@
+class Input
+    def user_input
+        @students_and_scores = {}
+        counter = 0 
+        
+        while counter < 25
+            puts "Enter student name: "
+            @student_name = gets.chomp
+            
+            puts "Enter first test score: "
+            @score1 = gets.chomp.to_i
+            
+            puts "Enter second test score: "
+            @score2 = gets.chomp.to_i
+            
+            puts "Enter third test score: "
+            @score3 = gets.chomp.to_i
+            
+            puts "Are you done entering student information?"
+            @exit_loop = gets.chomp.downcase
+            
+            # add students and their scores to hash
+            @students_and_scores[@student_name] = [@score1, @score2, @score3]
+            
+            break if @exit_loop == "yes"
+            
+            counter += 1
+        end
+    end
+end
+
+x = Input.new
+puts x.user_input
